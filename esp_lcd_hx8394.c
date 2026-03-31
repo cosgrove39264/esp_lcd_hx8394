a4691f58236a14bbec1bcccfdbf53736cf41a914
Marcel Schmidt <derprogger@gmail.com>
2026-03-31 12:46:02 +0200

    erste Anpassung auf IDF v6

--- esp_lcd_hx8394.c
+++ esp_lcd_hx8394.c
@@ -69,7 +69,7 @@ esp_err_t esp_lcd_new_panel_hx8394(const esp_lcd_panel_io_handle_t io, const esp
         ESP_GOTO_ON_ERROR(gpio_config(&io_conf), err, TAG, "configure GPIO for RST line failed");
     }
 
-    switch (panel_dev_config->color_space)
+    switch (panel_dev_config->rgb_ele_order)
     {
     case LCD_RGB_ELEMENT_ORDER_RGB:
         hx8394->madctl_val = 0;
